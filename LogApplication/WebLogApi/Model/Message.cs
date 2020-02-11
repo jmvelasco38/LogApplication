d9b8c2ff2d556c73319aaa754971e9e06a3846dd
JulianVelasco <jumavebri@gmail.com>
2020-02-11 04:59:46 -0500

    First add to Model Message Class

---
+++ LogApplication/WebLogApi/Model/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebLogApi.Model
+{
+    /// <summary>
+    /// Enumeración que contiene los tipos de mensaje que son manejados en el sistema de log
+    /// </summary>
+    public enum MessageType { Error, Warning }
+    /// <summary>
+    /// Clase que contiene los mensaje que serán enviados a través del sistema de log
+    /// </summary>
+    public class Message
+    {
+        /// <summary>
+        /// Indica el tipo de mensaje 
+        /// </summary>
+        public MessageType LogType { get; set; }
+        /// <summary>
+        /// Indica el conteni que será almacenado en el mensaje
+        /// </summary>
+        public string Content { get; set; }
+
+    }
+    
+}
